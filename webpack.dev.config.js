b1e6c4462bc5a5717da076498903581449bfaa5a
Alex Wilhelm <awwilhelm2014@gmail.com>
2017-09-26 14:05:51 -0500

    Basic startup

---
+++ webpack.dev.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  devServer: {
+    contentBase: './dist'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets : ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+        }, {
+          loader: "css-loader" // translates CSS into CommonJS
+        }, {
+          loader: "sass-loader" // compiles Sass to CSS
+        }]
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'My App',
+      template: './src/index.html',
+      filename: 'index.html',
+      inject: 'body'
+    })
+  ]
+};
